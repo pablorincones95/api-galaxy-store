0a5f96aa13afa5b1f9a1c91aabb4a822b53cc88b
Pablo Rincones <pablorincones95@gmail.com>
2025-05-21 11:46:54 -0400

    feat: agregar mΓö£Γöédulos y controladores para productos y categorΓö£┬ías

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './modules/products/products.module';
+import { CategoriesService } from './modules/categories/categories.service';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, CategoriesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CategoriesService],
 })
 export class AppModule {}
